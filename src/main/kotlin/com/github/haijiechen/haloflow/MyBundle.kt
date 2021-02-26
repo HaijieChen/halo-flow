d33707dee1e9878d5c52340110975e894d2ebf11
GitHub Action <action@github.com>
2021-02-26 10:47:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/haijiechen/haloflow/MyBundle.kt
+++ src/main/kotlin/com/github/haijiechen/haloflow/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.haijiechen.haloflow
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/haijiechen/haloflow/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/haijiechen/haloflow/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.haijiechen.haloflow.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.haijiechen.haloflow.MyBundle
 
 class MyApplicationService {
 
